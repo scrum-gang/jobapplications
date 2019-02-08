66a069c062c0c5367810bc0e79538fe3405d8d0c
Andrei <andrei.ungur@mail.mcgill.ca>
2019-02-08 12:34:04 -0500

    Add tests (wip)

---
+++ tests/test_db_creation.py
@@ -0,0 +1,30 @@
+import ...
+
+
+def test_db_is_created():
+    """
+    Ensures that the database is created and contains
+    the correct tables.
+    """
+    pass
+
+
+def test_db_can_add_season():
+    """
+    Ensures that it is possible to add a season inside the database
+    """
+    pass
+
+
+def test_db_can_add_inhouse_application():
+    """
+    Ensures that it is possible to add an inhouse application
+    """
+    pass
+
+
+def test_db_can_add_external_application():
+    """
+    Ensures that it is possible to add an external application
+    """
+    pass
