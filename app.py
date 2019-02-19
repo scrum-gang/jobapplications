741b3ecc8f19324f7413872769dda1d8d79698f9
Andrei <andrei.ungur@mail.mcgill.ca>
2019-02-18 23:03:58 -0500

    Fix up app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 from sqlalchemy import create_engine
 
-from external import apply_external_posting, update_status_external_posting, get_applications_external
-from internal import get_applications_internal
+from external import apply_external, update_status_external, get_applications_external
+from internal import apply_internal, update_status_internal, get_applications_internal
 from utils import app
 
 
@@ -11,24 +11,44 @@ def index():
   return "<h1> Hello World! </h1>"
 
 
-@app.route('/apply-external', methods=['POST'])
+@app.route('/apply/external', methods=['POST'])
 def apply_external():
   """
   Enables user to apply to an external job posting.
 
   Request body:
   - `url`: URL of the external posting
-  - `title`: Job title at the external posting
-  - `season`: Season during which the job takes place
+  - `position`: Job position of the external posting
+  - `company`: Company where job takes place
+  - `resume`: Handy tool for applying to jobs
+  - `date_posted`: When the application was posted
+  - `deadline`: Deadline to apply for the job
   - `user_id`: ID of the user applying
   """
   content = request.json
-  job_url, job_title, job_season = content['url'], content['title'], content['season']
-  user_id = content['user_id']
-  return apply_external_posting(user_id, job_url, job_title, job_season)
+  url, position, company = content['url'], content['position'], content['company']
+  date_posted, deadline = content['date_posted'], content['deadline']
+  user_id, resume = content['user_id'], content['resume']
+  return apply_external(user_id, url, position, company, resume, date_posted, deadline)
 
 
-@app.route('/update-status-external', methods=['POST'])
+@app.route('/apply/internal', methods=['POST'])
+def apply_internal():
+  """
+  Enables user to apply to an external job posting.
+
+  Request body:
+  - `user_id`: ID of the user applying
+  - `job_id`: ID of the job the user is applying to
+  - `resume`: Handy tool for applying to jobs
+  """
+  content = request.json
+  job_id = content['job_id']
+  user_id, resume = content['user_id'], content['resume']
+  return apply_external(user_id, job_id, resume)
+
+
+@app.route('/update-status/external', methods=['POST'])
 def update_status_external():
   """
   Updates the status of an external job posting
@@ -38,25 +58,47 @@ def update_status_external():
   - `new_status`: New status of the job application
   """
   content = request.json
-  job_id = content['id']
+  application_id = content['id']
   new_status = content['new_status']
-  return update_status_external_posting(job_id, new_status)
+  return update_status_external(application_id, new_status)
 
 
-@app.route('/get-external/<user_id>')
-def get_external(user_id):
+@app.route('/update-status/internal', methods=['POST'])
+def update_status_internal():
   """
-  Gets external job postings for a specific user.
+  Updates the status of an external job posting
+
+  Request body:
+  - `id`: Job application ID
+  - `new_status`: New status of the job application
   """
-  return get_applications_external(user_id)
+  content = request.json
+  application_id = content['id']
+  new_status = content['new_status']
+  return update_status_internal(application_id, new_status)
 
 
-@app.route('/get-internal/<job_id>')
+@app.route('/get/user/<user_id>')
+@app.route('/get/user/<user_id>/<application_type>')
+def get_application(user_id, application_type=None):
+  """
+  Gets job postings for a specific user.
+  """
+  applications_external, applications_internal = [], []
+  if application_type == "external" or not application_type:
+    applications_external = get_applications_external(user_id)
+  if application_type == "internal" or not application_type:
+    applications_internal = get_applications_internal(user_id, 'user')
+
+  return applications_external + applications_internal
+
+
+@app.route('/get/job/<job_id>')
 def get_internal(job_id):
   """
   Gets all job postings to an internal job
   """
-  return get_applications_internal(job_id)
+  return get_applications_internal(job_id, 'job')
 
 
 if __name__ == '__main__':
