60a91d269dfc2585eb1a98dd6fe966c8d7ea222b
Andrei <andrei.ungur@mail.mcgill.ca>
2019-02-18 23:13:06 -0500

    Remove useless print

--- utils.py
+++ utils.py
@@ -16,6 +16,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-print(app.config)
 db = SQLAlchemy(app)
 
