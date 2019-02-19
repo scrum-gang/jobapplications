f6c020e6fdaf395830d84913390acd39bc510c4b
Andrei <andrei.ungur@mail.mcgill.ca>
2019-02-19 00:56:44 -0500

    Reset heroku specific things

--- utils.py
+++ utils.py
@@ -10,11 +10,11 @@ app.debug = True
 
 
 # Config
-#db_name = os.environ.get("PSQL_USER", "")
-#db_pw = os.environ.get("PSQL_PW", "")
-#db_uri = f'postgresql://{db_name}:{db_pw}@localhost/jobapplications'
-#app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db_name = os.environ.get("PSQL_USER", "")
+db_pw = os.environ.get("PSQL_PW", "")
+db_uri = f'postgresql://{db_name}:{db_pw}@localhost/jobapplications'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 
